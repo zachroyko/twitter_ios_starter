57ac4f262c4420eb0186da0063c2967ced961917
Zach <zach@zachs-mbp.local>
2019-02-10 21:06:18 -0500

    Added tweet, favorite and retweet functionality

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -22,6 +22,11 @@ class HomeTableViewController: UITableViewController {
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
 
     @objc func loadTweets(){
         numberOfTweet = 20
@@ -84,6 +89,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweet(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     // MARK: - Table view data source
